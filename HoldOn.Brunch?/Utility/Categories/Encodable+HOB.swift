93a7bccc9bdc287d08956f832a2ba64d9474517c
Brett Petersen <bap034@gmail.com>
2019-10-11 18:31:07 -0700

    added cocoaPods + added FirebasePod + added HOBFirebase wrapper to handle storing and retrieving data to/from Firebase

--- HoldOn.Brunch?/Utility/Categories/Encodable+HOB.swift
+++ HoldOn.Brunch?/Utility/Categories/Encodable+HOB.swift
@@ -10,8 +10,18 @@ import Foundation
 
 extension Encodable {
 	func toJSONString() -> String? {
-		guard let jsonData = try? JSONEncoder().encode(self) else { return nil }
+		guard let jsonData = toJSONData() else { return nil }
 		let jsonString = String(data: jsonData, encoding: .utf8)
 		return jsonString
+	}
+	func toJSONData() -> Data? {
+		let jsonData = try? JSONEncoder().encode(self)
+		return jsonData
+	}
+	func toJSON() -> [String: Any]? {
+		guard let jsonData = toJSONData() else { return nil }
+		
+		let json = try? JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as? [String: Any]
+		return json
 	}
 }
---
+++ Pods/Target Support Files/Pods-HoldOn.Brunch?Tests/Pods-HoldOn.Brunch?Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HoldOn_Brunch_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HoldOn_Brunch_Tests
+@end
