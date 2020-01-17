47f6e5ccefad2d031f51861d5b645889755a1667
Brett Petersen <bap034@gmail.com>
2020-01-16 18:02:52 -0800

    updated functions/index.js to use tabs

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ var topic = 'allUsers';
 	var message = {
 		notification: {
 			title: titleText,
-			body: messageText	
+			body: messageText
 		},
 		topic: topic
 	};
@@ -37,8 +37,8 @@ var topic = 'allUsers';
 
 // Listen for changes in all documents in the 'users/messages' collection and sends a push notification 
 exports.sendNotificationOnMessageCreate = functions.firestore
-    .document('persons/{personId}/messages/{messageId}')
-    .onCreate((snap, context) => {
+	.document('persons/{personId}/messages/{messageId}')
+	.onCreate((snap, context) => {
 	// Get an object representing the document
 	// e.g. {'name': 'Marie', 'age': 66}
 	// const newValue = snap.data();
@@ -50,19 +50,54 @@ exports.sendNotificationOnMessageCreate = functions.firestore
 	let personRef = db.collection('persons').doc(personId);
 	personRef.get()
 	.then(doc => {
-	    if (!doc.exists) {
-	      console.log('No such document!');  
-	      return nil;          
-	    } else {
+		if (!doc.exists) {
+			console.log('No such document!');
+			return nil;
+		} else {
 			console.log('Document data:', doc.data());
 			const name = doc.data().name;
-	      	const titleText = `${name} said...`;
+			const titleText = `${name} said...`;
 			const messageText = snap.get('text');
 
 			sendMessageToAllUsers(titleText, messageText);
 
 			return doc;
-	    }
+		}
+	})
+	.catch(err => {
+		console.log('Error getting document', err);
+		return err;
+	});
+});
+
+	// Listen for changes in all documents in the 'users/messages' collection and sends a push notification 
+exports.sendNotificationOnMessageCreate = functions.firestore
+	.document('persons/{personId}/messages/{messageId}')
+	.onCreate((snap, context) => {
+	// Get an object representing the document
+	// e.g. {'name': 'Marie', 'age': 66}
+	// const newValue = snap.data();
+
+	// access a particular field as you would any JS property
+	const personId = snap.get('personId');
+
+	
+	let personRef = db.collection('persons').doc(personId);
+	personRef.get()
+	.then(doc => {
+		if (!doc.exists) {
+			console.log('No such document!');  
+			return nil;
+		} else {
+			console.log('Document data:', doc.data());
+			const name = doc.data().name;
+			const titleText = `${name} said...`;
+			const messageText = snap.get('text');
+
+			sendMessageToAllUsers(titleText, messageText);
+
+			return doc;
+		}
 	})
 	.catch(err => {
 		console.log('Error getting document', err);
