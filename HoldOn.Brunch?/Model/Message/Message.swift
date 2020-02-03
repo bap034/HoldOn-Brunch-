9b2b0e176f74122f80eab4501dfbca5da477e29e
Brett Petersen <bap034@gmail.com>
2020-02-02 21:47:49 -0800

    added reactionTypes to Message model to allow for storing data of reactions to a specific message + added images for each reactionType + update MessageCellView to display a placeholder image and any reactionType Images

--- HoldOn.Brunch?/Model/Message/Message.swift
+++ HoldOn.Brunch?/Model/Message/Message.swift
@@ -8,6 +8,56 @@
 
 import Foundation
 
+enum MessageReactionType: String, Codable, Identifiable {
+	case bread
+	case coffee
+	case cupcake
+	case egg
+	case frenchFries
+	case iceCream
+	case pizza
+	case pumpkin
+	case soda
+	case steak
+	case sushi
+	case taco
+	
+	var id: String { return rawValue }
+	
+	func getImageString() -> String {
+		let imageString: String
+		
+		switch self {
+		case .bread:
+			imageString = "icons8-kawaii-bread"
+		case .coffee:
+			imageString = "icons8-kawaii-coffee"
+		case .cupcake:
+			imageString = "icons8-kawaii-cupcake"
+		case .egg:
+			imageString = "icons8-kawaii-egg"
+		case .frenchFries:
+			imageString = "icons8-kawaii-french-fries"
+		case .iceCream:
+			imageString = "icons8-kawaii-ice-cream"
+		case .pizza:
+			imageString = "icons8-kawaii-pizza"
+		case .pumpkin:
+			imageString = "icons8-kawaii-pumpkin"
+		case .soda:
+			imageString = "icons8-kawaii-soda"
+		case .steak:
+			imageString = "icons8-kawaii-steak"
+		case .sushi:
+			imageString = "icons8-kawaii-sushi"
+		case .taco:
+			imageString = "icons8-kawaii-taco"
+		}
+		
+		return imageString
+	}
+}
+
 struct Message: ModelProtocol, Identifiable {
 	var id: String {
 		let dateFormatter = ISO8601DateFormatter()
@@ -18,6 +68,7 @@ struct Message: ModelProtocol, Identifiable {
 	let personId: String
 	let created: Date
 	let text: String
+	var reactionTypes: [MessageReactionType]? = nil
 	
 	var displayDate: String {
 		let dateFormatter = ISO8601DateFormatter()
