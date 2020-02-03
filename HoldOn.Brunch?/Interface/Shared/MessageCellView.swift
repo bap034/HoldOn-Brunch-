60dc838213e9062c6ed50d4b7a29de89f6a9b0ca
Brett Petersen <bap034@gmail.com>
2020-02-02 21:47:49 -0800

    added reactionTypes to Message model to allow for storing data of reactions to a specific message + added images for each reactionType + update MessageCellView to display a placeholder image and any reactionType Images

--- HoldOn.Brunch?/Interface/Shared/MessageCellView.swift
+++ HoldOn.Brunch?/Interface/Shared/MessageCellView.swift
@@ -11,26 +11,52 @@ import SwiftUI
 struct MessageCellView: View {
 	
 	var message: Message
+	var sureMessageReactionTypes: [MessageReactionType] {
+		let reactionTypes = message.reactionTypes ?? []
+		return reactionTypes
+	}
 	
     var body: some View {
-		HStack {
-			VStack {
-				Text(message.displayDate)
-					.font(.caption)
-					.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-					.foregroundColor(.gray)
-				Text(message.text)
-					.font(.body)
-					.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+		VStack(spacing: 0) {
+			HStack {
+				VStack {
+					Text(message.displayDate)
+						.font(.caption)
+						.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+						.foregroundColor(.gray)
+					Text(message.text)
+						.font(.body)
+						.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+				}
+				
+				Spacer()
+				
+				Image("icons8-kawaii-pizza-outline")
+					.resizable()
+					.scaledToFit()
+					.frame(width: 50, height: 50)
 			}
-			Spacer()
+			
+			if !sureMessageReactionTypes.isEmpty {
+				HStack(spacing: 0) {
+					Spacer()
+					
+					ForEach(sureMessageReactionTypes) { reactionType in
+						Image(reactionType.getImageString())
+							.resizable()
+							.scaledToFit()
+							.frame(maxWidth: 30, maxHeight: 30)
+					}
+				}
+			}
 		}
 	}
 }
 
 struct MessageCellView_Previews: PreviewProvider {
     static var previews: some View {
-		let message = Message(personId: "Baller", created: Date(), text: "Message text")
+		var message = Message(personId: "Baller", created: Date(), text: "Message text")
+		message.reactionTypes = [.pizza, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream, .iceCream]
 		return MessageCellView(message: message)
     }
 }
